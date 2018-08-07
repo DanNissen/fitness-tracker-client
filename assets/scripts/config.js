c66aa31ce8121b5c858e11528d9db25835ad8799
DanNissen <d.edwardnissen@gmail.com>
2018-08-07 11:21:42 -0400

    Add Sign Up Features
    Create auth directory
    create authEvents authApi & authUi files
    Add message modal to index.HTML
    Update signUp Modal
    update deployed api url in config
    index.js has signUp submit event listener
    authEvents handles submit event
    auth api sends ajax request to api
    authUi displays success or failure to user

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://fitness-tracker-rails-api.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
